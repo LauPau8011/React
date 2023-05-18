4b8b9153a0ddff088ad0da38c372d703464ef738
Laupau8011 <laura.januleviciute@gmail.com>
2023-05-18 21:05:30 +0300

    Products.jsx ir Product.jsx pridėjimas

---
+++ 2_paskaita/my-app/src/components/Car.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { brand: "Ford", model: "Mustang", color: "red", year: 1964 };
+  }
+  chngeColor = () => {
+    this.state({ color: "blue" });
+  };
+  render() {
+    return (
+      <div>
+        <h1>My car {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color} {this.state.model} from {this.state.year}
+        </p>
+        <button onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }
+}
+
+export default Car;
