6473c00038722f7c6f26ce5370d28b15c7575368
Laupau8011 <laura.januleviciute@gmail.com>
2023-05-22 19:06:32 +0300

    Added rate in product.jsx

--- 2_paskaita/mano-aplikacija/src/components/FuncCounter.jsx
+++ 2_paskaita/mano-aplikacija/src/components/FuncCounter.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 
 const FuncCounter = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); //naudojama useState hooks funkcija iš React, kad būtų galima laikyti skaičiaus
+  // būseną ir atnaujinti ją. Pradinė count būsena nustatoma į 0.
 
   const increment = () => {
     setCount(count + 1);
@@ -10,13 +11,16 @@ const FuncCounter = () => {
   const decrement = () => {
     setCount(count - 1);
   };
-
+  const reset = () => {
+    setCount(0);
+  };
   return (
     <div>
       <h2>Counter</h2>
       <p>Count: {count}</p>
       <button onClick={increment}>Increase</button>
       <button onClick={decrement}>Decrease</button>
+      <button onClick={reset}>Reset count</button>
     </div>
   );
 };
