eddff5a609aef5bab55cbd2447b4605e32ea50fe
Laupau8011 <laura.januleviciute@gmail.com>
2023-05-22 22:11:31 +0300

    Hero, Button, MoodChecker panaikinimas

--- 2_paskaita/mano-aplikacija/src/App.js
+++ 2_paskaita/mano-aplikacija/src/App.js
@@ -3,12 +3,6 @@ import FuncCar from './components/FuncCar';
 import FuncBestCar from './components/FuncBestCar';
 import FuncCounter from './components/FuncCounter';
 import Counter from './components/CounterClass';
-import Hero from './components/Hero';
-import Button from './components/Button';
-import MoodChecker from './components/MoodChecker';
-import MoodCheckerV2 from './components/MoodCheckerV2';
-import './App.css';
-import MoodCheckerV3 from './components/MoodCheckerV3';
 
 const App = () => {
   return (
@@ -25,27 +19,6 @@ const App = () => {
       <FuncBestCar />
       <FuncCounter />
       <Counter />
-      <div>
-        <Hero title="Info hero" subtitle="Info subtitle" />
-        <br />
-        <Hero title="Info hero" />
-        <br />
-        <Hero subtitle="Info subtitle" />
-      </div>
-      <div>
-        <Button title="TEXT" variant="text" />
-        <Button title="CONTAINED" variant="contained" />
-        <Button title="OUTLINED" variant="outlined" />
-      </div>
-      <div>
-        <MoodChecker />
-      </div>
-      <div>
-        <MoodCheckerV2 />
-      </div>
-      <div>
-        <MoodCheckerV3 />
-      </div>
     </div>
   );
 };
